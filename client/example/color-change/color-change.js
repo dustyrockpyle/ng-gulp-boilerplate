5d7dfa8a70814a2d7b10a50b439557695a9aa2b7
dustyrockpyle <dustyrockpyle@gmail.com>
2015-01-31 19:35:48 -0500

    Added tests for color-change directive.

--- client/example/color-change/color-change.js
+++ client/example/color-change/color-change.js
@@ -1,5 +1,5 @@
-angular.module('example.color-change', [])
-  .directive('colorChange', function($interval){
+angular.module('example.color-change', ['templates', 'ng.lodash'])
+  .directive('colorChange', function($interval, _, $timeout){
     return {
       restrict: 'E',
       scope: {
@@ -7,24 +7,25 @@ angular.module('example.color-change', [])
       },
       templateUrl: 'color-change.tpl.html',
       transclude: true,
-      controller: function($scope, $element, $interval, _){
+      link: function(scope, element){
         var compiled = _.template("rgb({{r}}, {{g}}, {{b}})");
         var fraction = 1.0;
-        var diff = -.02;
+        var diff = -0.02;
         var setColor = function() {
-          $scope.color = compiled({
-            r: Math.round($scope.rgb[0] * fraction),
-            g: Math.round($scope.rgb[1] * fraction),
-            b: Math.round($scope.rgb[2] * fraction)
+          scope.color = compiled({
+            r: Math.round(scope.rgb[0] * fraction),
+            g: Math.round(scope.rgb[1] * fraction),
+            b: Math.round(scope.rgb[2] * fraction)
           });
-          var spans = $element[0].getElementsByTagName('span');
-          spans[0].style.color = $scope.color;
-          spans[1].style.color = $scope.color;
+          var spans = element.find('span');
+          spans[0].style.color = scope.color;
+          spans[1].style.color = scope.color;
           fraction += diff;
-          if(fraction <= .3 || fraction >= 1.0){
+          if(fraction <= 0.3 || fraction >= 1.0){
             diff = -diff;
           }
         };
+        setColor();
         $interval(setColor, 100);
       }
     };
--- client/example/dummy-test.spec.js
+++ client/example/dummy-test.spec.js
@@ -1,6 +1,6 @@
-describe( 'dummy test', function() {
+describe('dummy test', function() {
   beforeEach(module('example'));
-  it( 'ensure the module loads correctly', inject(function() {
+  it('ensure the module loads correctly', inject(function() {
     expect(true).toBeTruthy();
   }));
 });
