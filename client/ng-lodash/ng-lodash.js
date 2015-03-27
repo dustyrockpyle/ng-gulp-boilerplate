232e9ecaefa5a5c6b192101a566808c5a887195d
dustyrockpyle <dustyrockpyle@gmail.com>
2015-03-27 16:31:59 -0400

    Updated dependencies and added more granular gulp tasks.

--- client/ng-lodash/ng-lodash.js
+++ client/ng-lodash/ng-lodash.js
@@ -1,5 +1,5 @@
 angular.module('ng.lodash', []).
-    factory('_', function($window){
-        $window._.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
-        return $window._;
-    });
+  factory('_', function($window){
+    $window._.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
+    return $window._;
+  });
